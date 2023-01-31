874361b66dc5a365b3034f845e3fc272d7a408d4
DanlexSV <danilibra15@hotmail.com>
2023-01-31 13:46:40 +0100

    menu

--- Reto Alumnos/src/notas.java
+++ Reto Alumnos/src/notas.java
@@ -4,50 +4,95 @@ import javax.swing.JOptionPane;
 
 public class notas {
 
+	static alumno a = new alumno();
+	static util u = new util();
+	static ArrayList<alumno> listaAlumnos = new ArrayList<alumno>();
+	static String NombreFichero = null;
+	static gestionFichero gf = new gestionFichero(NombreFichero);
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		alumno a = new alumno();
-		ArrayList<alumno> listaAlumnos = new ArrayList<alumno>();
-		String NombreFichero = null;
-		gestionFichero gf = new gestionFichero(NombreFichero);
 		String opt = JOptionPane.showInputDialog("A ~ Alta al alumno" + "\n" + "L ~ Listar las notas" + "\n"
 				+ "B ~ Borrar nota" + "\n" + "M ~ Modificar una nota" + "\n" + "C ~ Consultar las notas" + "\n"
 				+ "S ~ Salir" + "\n" + "Escoja una de las opciones").toUpperCase();
-		while (!opt.equals("S"))
+		while (!opt.equals("S")) {
 			switch (opt) {
 			case "A":
-				NombreFichero = JOptionPane.showInputDialog("Escribe el nombre del fichero donde quieres guardas las notas");
-				a.setDNIAlumno(JOptionPane.showInputDialog("Escriba el DNI del alumno"));
-				a.setNombreAlumno(JOptionPane.showInputDialog("Escriba el nombre del alumno"));
-				a.setCicloformativo(JOptionPane.showInputDialog("Escriba el ciclo formativo actual"));
-				a.setModuloAlumno(JOptionPane.showInputDialog("Escriba el modulo de la nota"));
-				a.setNotaAlumno(JOptionPane.showInputDialog("Escriba la nota que va a subir"));
-				listaAlumnos.add(a);
+				DarAlta();
 				gf.escribir(a);
 				break;
-			case "L": 
-				NombreFichero = JOptionPane.showInputDialog("Escribe el nombre del fichero de donde quieres ver las notas");
-				File file = new File(NombreFichero);
-				if (file.exists())
-					gf.leer();
-				else 
-					JOptionPane.showMessageDialog(null, "El fichero no existe つ ◕_◕ つ");
+			case "L":
+				Listado();
 				break;
 			case "B":
-				util u = new util();
-				NombreFichero = JOptionPane.showInputDialog("Escribe el nombre del fichero de donde quieres ver las notas");
-				File fil = new File(NombreFichero);
-				if (fil.exists()) {
-					a.setDNIAlumno(JOptionPane.showInputDialog("Escribe el nombre del alumno que deseas eliminar"));
-					u.borrar(listaAlumnos, a);
-				}
-				else 
-					JOptionPane.showMessageDialog(null, "Este fichero no existe つ ◕_◕ つ");
+				Borrar();
 				break;
 			case "M":
+				Modificar();
+				break;
 			case "C":
+				Consultar();
+				break;
 			default:
 			}
+			opt = JOptionPane.showInputDialog("A ~ Alta al alumno" + "\n" + "L ~ Listar las notas" + "\n"
+					+ "B ~ Borrar nota" + "\n" + "M ~ Modificar una nota" + "\n" + "C ~ Consultar las notas" + "\n"
+					+ "S ~ Salir" + "\n" + "Escoja una de las opciones").toUpperCase();
+		}
+	}
+
+	private static void Consultar() {
+		NombreFichero = JOptionPane.showInputDialog("Escriba el nombre del alumno que deseas consultar");
+		File file = new File(NombreFichero);
+		if (file.exists()) {
+			a.setModuloAlumno(JOptionPane.showInputDialog("Escriba el Modulo que queire consultar las notas"));
+			listaAlumnos.add(a);
+			u.consultar(listaAlumnos, a);
+		} else
+			JOptionPane.showMessageDialog(null, "Este fichero no existe つ ◕_◕ つ");
+	}
+
+	private static void Modificar() {
+		NombreFichero = JOptionPane.showInputDialog("Escribe el nombre del alumno que deseas modificar");
+		File file = new File(NombreFichero);
+		if (file.exists()) {
+			a.setDNIAlumno(JOptionPane.showInputDialog("Escrina el DNI del alumno"));
+			a.setModuloAlumno(JOptionPane.showInputDialog("Escriba el Modulo donde quiere cambiar la nota"));
+			a.setNotaAlumno(JOptionPane.showInputDialog("Escriba el valor de la nueva nota"));
+			listaAlumnos.add(a);
+			u.modificar(listaAlumnos, a);
+		} else
+			JOptionPane.showMessageDialog(null, "Este fichero no existe つ ◕_◕ つ");
+	}
+
+	private static void Borrar() {
+		NombreFichero = JOptionPane.showInputDialog("Escribe el nombre del fichero de donde quieres ver las notas");
+		File file = new File(NombreFichero);
+		if (file.exists()) {
+			a.setDNIAlumno(JOptionPane.showInputDialog("Escribe el DNI del alumno que deseas eliminar"));
+			listaAlumnos.add(a);
+			u.borrar(listaAlumnos, a);
+		} else
+			JOptionPane.showMessageDialog(null, "Este fichero no existe つ ◕_◕ つ");
+	}
+
+	private static void Listado() {
+		NombreFichero = JOptionPane.showInputDialog("Escribe el nombre del fichero de donde quieres ver las notas");
+		File file = new File(NombreFichero);
+		if (file.exists())
+			gf.leer();
+		else
+			JOptionPane.showMessageDialog(null, "El fichero no existe つ ◕_◕ つ");
+	}
+
+	private static void DarAlta() {
+		NombreFichero = JOptionPane.showInputDialog("Escribe el nombre del fichero donde quieres guardas las notas");
+		a.setDNIAlumno(JOptionPane.showInputDialog("Escriba el DNI del alumno"));
+		a.setNombreAlumno(JOptionPane.showInputDialog("Escriba el nombre del alumno"));
+		a.setCicloformativo(JOptionPane.showInputDialog("Escriba el ciclo formativo actual"));
+		a.setModuloAlumno(JOptionPane.showInputDialog("Escriba el modulo de la nota"));
+		a.setNotaAlumno(JOptionPane.showInputDialog("Escriba la nota que va a subir"));
+		listaAlumnos.add(a);
 	}
 
 }
--- Reto Alumnos/src/util.java
+++ Reto Alumnos/src/util.java
@@ -3,40 +3,59 @@ import javax.swing.JOptionPane;
 
 public class util {
 	gestionFichero g;
+
 	public void printar(ArrayList<alumno> misnotas) {
 		String texto = "";
-		for (alumno notas: misnotas)
+		for (alumno notas : misnotas)
 			texto += notas + "\n";
 		JOptionPane.showMessageDialog(null, texto);
 	}
+
 	public boolean borrar(ArrayList<alumno> misnotas, alumno a) {
 		boolean haBorrado = true;
 		try {
 			misnotas.clear();
 			g.leer();
-			for (int i = 0; i < misnotas.size(); i++){
+			for (int i = 0; i < misnotas.size(); i++) {
 				if (misnotas.get(i).equals(a))
 					misnotas.remove(i);
-				else 
+				else
 					JOptionPane.showMessageDialog(null, "No existe el DNI solicitado つ ◕_◕ つ");
 			}
 		} catch (Exception e) {
 			// TODO: handle exception
-			haBorrado = false; 
+			haBorrado = false;
 		}
 		return haBorrado;
 	}
-	public boolean modificar(ArrayList<alumno> misnotas, alumno Modulo) {
+
+	public boolean modificar(ArrayList<alumno> misnotas, alumno a) {
 		boolean haModificado = true;
 		try {
 			misnotas.clear();
 			g.leer();
-			
+			for (int i = 0; i < misnotas.size(); i++) {
+				if (misnotas.get(i).equals(a)) {
+					misnotas.remove(i);
+					g.escribir(a);
+				} else 
+					JOptionPane.showMessageDialog(null, "No existe el DNI solicitado つ ◕_◕ つ");
+			}
 		} catch (Exception e) {
 			// TODO: handle exception
 			return haModificado = false;
 		}
-		
 		return haModificado;
+	}
+	
+	public void consultar(ArrayList<alumno> misnotas, alumno a) {
+		misnotas.clear();
+		g.leer();
+		for (int i = 0; i < misnotas.size(); i++) {
+			if (misnotas.get(i).equals(a)) {
+				JOptionPane.showMessageDialog(null, a);
+			} else
+				JOptionPane.showMessageDialog(null, "No existe el Modulo solicitado つ ◕_◕ つ");
+		}
 	}
 }
