57adbf3257862597334c4431721f2e99b0d2070d
DanlexSV <danilibra15@hotmail.com>
2023-03-13 10:15:24 +0100

    last

--- Electrodomesticos/src/Tienda.java
+++ Electrodomesticos/src/Tienda.java
@@ -4,10 +4,11 @@ public class Tienda {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String nombre, color;
+		/*String nombre, color;
 		int peso, cantidad;
 		char consumo;
-		double precio;
+		double precio;*/
+		Electrodomestico e = new Electrodomestico();
 		JOptionPane.showMessageDialog(null, "Hola! Bienvenido \n" + 
 				"a continuecion escriba los productos de su tienda");
 		String ask = JOptionPane.showInputDialog("Quieres agregar un producto nuevo? \n"
@@ -21,6 +22,10 @@ public class Tienda {
 		GestionFichero gf = new GestionFichero(JOptionPane.showInputDialog("Escribe el nombre del fichero"));
 		while (opt.equals("S")) {
 			switch(opt) {
+			case "A": 
+				Alta(e);
+				gf.escribir(e);
+				break;
 			case "L": 
 			default: 
 				JOptionPane.showMessageDialog(null, "Valor introducido no reconocible \n" 
@@ -32,7 +37,7 @@ public class Tienda {
 		}
 	}
 	
-	private void Alta(Electrodomestico e) {
+	private static void Alta(Electrodomestico e) {
 		e.setNombre(JOptionPane.showInputDialog("Escribe el nombre del producto"));
 		e.setPrecio_base(Double.parseDouble(JOptionPane.showInputDialog("Escribe su precio base")));
 		e.setPeso(Integer.parseInt(JOptionPane.showInputDialog("Escribe su peso")));
